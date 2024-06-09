a868ecadb918cf79f1f175cf347658f891b3369c
pstryk1 <150193642+pstryk1@users.noreply.github.com>
2024-06-09 12:32:01 +0200

    Plik z theme quocalcus

---
+++ user.py
@@ -0,0 +1,23 @@
+USER_THEMES = {
+    "quocalcus": {
+        "type": "dark",
+        "colors": {
+            "primary": "#39839f",
+            "secondary": "#4e5d6c",
+            "success": "#5cb85c",
+            "info": "#5bc0de",
+            "warning": "#f0ad4e",
+            "danger": "#d9534f",
+            "light": "#ABB6C2",
+            "dark": "#20374C",
+            "bg": "#080b14",
+            "fg": "#1897c0",
+            "selectbg": "#526170",
+            "selectfg": "#ffffff",
+            "border": "#20637c",
+            "inputfg": "#ebebeb",
+            "inputbg": "#32465a",
+            "active": "#2B4155"
+        }
+    }
+}
